7443a14e5486377d57b5becda82053e10686e1ce
NeelBollapragada <neelbollapragada@gmail.com>
2025-07-10 17:39:08 +0100

    reset project, added nativewind to jsx finally

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-3xl text-blue-500">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
