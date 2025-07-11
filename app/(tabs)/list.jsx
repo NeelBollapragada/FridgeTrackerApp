cb98a7a77731036b56d22ba91043cd8cc6fd47e0
NeelBollapragada <neelbollapragada@gmail.com>
2025-07-11 11:17:57 +0100

    added clear button to shopping list

--- app/(tabs)/list.jsx
+++ app/(tabs)/list.jsx
@@ -1,14 +1,66 @@
 import paperBg from "@/assets/images/lined-paper-3.png";
-import { ImageBackground, StyleSheet, TextInput } from "react-native";
+import { useNavigation } from "expo-router";
+import { useLayoutEffect, useState } from "react";
+import {
+  Alert,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 const List = () => {
+  const navigation = useNavigation();
+  const [text, setText] = useState("");
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          className="mr-3"
+          onPress={() => {
+            if (text.length > 0) {
+              clearList();
+            }
+          }}
+        >
+          <Text className="text-black font-medium px-3 py-2">CLEAR</Text>
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation, text]);
+
+  const clearList = () => {
+    Alert.alert(
+      "Clear Shopping List",
+      "Are you sure you want to clear your shopping list",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Clear",
+          style: "destructive",
+          onPress: () => {
+            setText("");
+          },
+        },
+      ]
+    );
+  };
+
   return (
     <ImageBackground source={paperBg} className="flex-1 bg-yellow-100">
       <TextInput
         placeholder="Add your items here..."
+        value={text}
+        onChangeText={setText}
         multiline={true}
+        textAlignVertical="top"
         autoCapitalize="none"
-        className="text-black mx-10 my-12 text-xl"
+        className="text-black mx-10 mt-12 text-xl"
       />
     </ImageBackground>
   );
