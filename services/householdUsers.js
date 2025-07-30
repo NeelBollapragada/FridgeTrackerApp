6fab987beda36c531b7d0fea8c63b8351356b90a
NeelBollapragada <neelbollapragada@gmail.com>
2025-07-30 15:14:55 +0100

    made household section in profile - need to add activity indicator

--- services/householdUsers.js
+++ services/householdUsers.js
@@ -6,6 +6,8 @@ const USERNAME_CHECK_URL =
 const USER_ADD_URL = "https://fridgetrackerbackend.onrender.com/api/users/add";
 const HOUSEHOLD_CREATE =
   "https://fridgetrackerbackend.onrender.com/api/household/create";
+const HOUSEHOLD_MEMBERS =
+  "https://fridgetrackerbackend.onrender.com/api/household/members";
 
 export const checkUserName = async (name) => {
   const online = await checkNetwork();
@@ -82,12 +84,43 @@ export const createHousehold = async () => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ userId: user.$id }),
+      body: JSON.stringify({ userId: user.$id, username: user.name }),
     });
 
     const code = await res.json();
     return code;
   } catch (error) {
     console.error("Error caught creating household", error);
+  }
+};
+
+export const getHouseholdMembers = async () => {
+  const online = await checkNetwork();
+  if (!online) {
+    console.warn("No internet connection. Skipping get household members.");
+    return false;
+  }
+
+  try {
+    const user = await account.get();
+    const res = await fetch(HOUSEHOLD_MEMBERS, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userId: user.$id }),
+    });
+
+    if (!res.ok) {
+      const data = await res.json();
+      console.error("Error fetching household members", data.error);
+      return;
+    }
+
+    const members = await res.json();
+    return members;
+  } catch (error) {
+    console.error("Error caught fetching members", error);
+    return false;
   }
 };
